718298a0626ed4a011549115410b84d8306a3451
Wondimu Zegeye <wondimuk@gmail.com>
2023-02-13 21:13:05 -0500

    Initial commit

---
+++ test/usElection.ts
@@ -0,0 +1,69 @@
+import { USElection__factory } from "./../typechain-types/factories/Election.sol/USElection__factory";
+import { USElection } from "./../typechain-types/Election.sol/USElection";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("USElection", function () {
+  let usElectionFactory;
+  let usElection: USElection;
+
+  before(async () => {
+    usElectionFactory = await ethers.getContractFactory("USElection");
+
+    usElection = await usElectionFactory.deploy();
+
+    await usElection.deployed();
+  });
+
+  it("Should return the current leader before submit any election results", async function () {
+    expect(await usElection.currentLeader()).to.equal(0); // NOBODY
+  });
+
+  it("Should return the election status", async function () {
+    expect(await usElection.electionEnded()).to.equal(false); // Not Ended
+  });
+
+  it("Should submit state results and get current leader", async function () {
+    const stateResults = ["California", 1000, 900, 32];
+
+    const submitStateResultsTx = await usElection.submitStateResult(
+      stateResults
+    );
+
+    await submitStateResultsTx.wait();
+
+    expect(await usElection.currentLeader()).to.equal(1); // BIDEN
+  });
+
+  it("Should throw when try to submit already submitted state results", async function () {
+    const stateResults = ["California", 1000, 900, 32];
+
+    expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+      "This state result was already submitted!"
+    );
+  });
+
+  it("Should submit state results and get current leader", async function () {
+    const stateResults = ["Ohaio", 800, 1200, 33];
+
+    const submitStateResultsTx = await usElection.submitStateResult(
+      stateResults
+    );
+
+    await submitStateResultsTx.wait();
+
+    expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+  });
+
+  it("Should end the elections, get the leader and election status", async function () {
+    const endElectionTx = await usElection.endElection();
+
+    await endElectionTx.wait();
+
+    expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+
+    expect(await usElection.electionEnded()).to.equal(true); // Ended
+  });
+
+  //TODO: ADD YOUR TESTS
+});
